35137e15aa12604b9f232ea9364728c9dce31515
shw <dustmzmfla@naver.com>
2026-02-07 14:24:54 +0900

    강사소개 멈춤 포인트 수정 및 "반 추천" -> "반배정" 텍스트 수정

--- src/pages/home/components/Instructor.tsx
+++ src/pages/home/components/Instructor.tsx
@@ -4,7 +4,7 @@ import { INSTRUCTORS } from '@/constants';
 
 const Instructor = memo(function Instructor() {
   return (
-    <Section id="instructor" background="gray" ariaLabelledby="instructor-title" className='max-md:pt-14 md:pt-14 md:pb-0'>
+    <Section id="instructor" background="gray" ariaLabelledby="instructor-title" className='max-md:pt-14 md:pt-20 md:pb-0'>
       <Container>
         <div className="text-center mb-14 max-md:mb-10">
           <h2 id="instructor-title" className="text-3xl md:text-4xl mb-2 tracking-title">
